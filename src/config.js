2e4e0019e92090891d02ac80745ec183d8370c11
Lu Yi-Te <jikotw74@gmail.com>
2017-04-29 20:41:57 +0800

    feat: init firebase.js

---
+++ src/config.js
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+	apiKey: "AIzaSyAzmYVPQ0ciJPLZrU3MqBfBTCw11eh3BJA",
+	authDomain: "moment-c7f66.firebaseapp.com",
+	databaseURL: "https://moment-c7f66.firebaseio.com",
+	projectId: "moment-c7f66",
+	storageBucket: "moment-c7f66.appspot.com",
+	messagingSenderId: "234565228463"
+};
