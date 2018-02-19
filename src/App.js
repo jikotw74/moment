d88d8e54d9ffb80348e9f6680527dc6551fda8a4
Deep <jikotw74@gmail.com>
2018-02-19 15:08:52 +0800

    fix update new version

--- src/App.js
+++ src/App.js
@@ -4,21 +4,77 @@ import './App.css';
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Main from './component/Main';
+import Home from './container/Home';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+
+import { Route, BrowserRouter, Link, Redirect, Switch } from 'react-router-dom'
+
+import { firebaseAuth } from './util/firebase.js'
 
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin();
 
+function PrivateRoute ({component: Component, authed, ...rest}) {
+  return (
+    <Route
+      {...rest}
+      render={(props) => authed === true
+        ? <Component {...props} />
+        : <Redirect to={{pathname: '/', state: {from: props.location}}} />}
+    />
+  )
+}
+
+function PublicRoute ({component: Component, authed, ...rest}) {
+  return (
+    <Route
+      {...rest}
+      render={(props) => authed === false
+        ? <Component {...props} />
+        : <Redirect to='/dashboard' />}
+    />
+  )
+}
+
 class App extends Component {
-  componentDidMount(){
+    constructor(props) {
+        super(props);
+        this.state = {
+            authed: false,
+            loading: true,
+        };
+    }
+    componentDidMount () {
+        this.removeListener = firebaseAuth().onAuthStateChanged((user) => {
+            if (user) {
+                this.setState({
+                authed: true,
+                loading: false,
+                })
+            } else {
+                this.setState({
+                    authed: false,
+                    loading: false
+                })
+            }
+        })
+    }
+  componentWillUnmount () {
+    this.removeListener()
   }
 
   render() {
     return (
       <div className="App">
         <MuiThemeProvider>
-          <Main />
+          <BrowserRouter>
+            <Switch>
+                <Route path='/' exact component={Home} />
+                <PrivateRoute authed={this.state.authed} path='/main' component={Main} />
+                <Route render={() => <h3>No Match</h3>} />
+              </Switch>
+          </BrowserRouter>
         </MuiThemeProvider>
       </div>
     );
---
+++ src/container/Home.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+	constructor(props) {
+        super(props);
+        this.state = {
+	    };
+    }
+
+    componentWillMount() {
+    }
+
+    render() {
+
+        return ( 
+            <div className="Home">
+		  		<div>Login</div>
+		  	</div>
+        );
+    }
+}
+
+export default Home;
